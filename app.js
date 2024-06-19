a57c056de6d00c48cfe021a18fb58815c42bf418
Nick Konstantinov <nick.konstantinov.job@gmail.com>
2024-06-19 14:35:58 +0400

    feat: add main block

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Burger Menu
 const burgerBtn = document.querySelector('.btn--burger');
 const burgerMenu = document.querySelector('.mobile-overlay');
+const wrapper = document.querySelector('.wrapper');
 
 burgerBtn.addEventListener('click', function(){
 	burgerBtn.classList.toggle('active');
 	burgerMenu.classList.toggle('open');
     document.body.classList.toggle("noscroll");
+	wrapper.classList.toggle("noscroll");
 });
 
 // Lang Menu
@@ -15,5 +17,17 @@ const langList = document.querySelectorAll('[data-lang]');
 langList.forEach(item => {
 	item.addEventListener('click', function(){
 		langBtnText.innerHTML = item.innerHTML;
+	})
+});
+
+// Blog
+const blogList = document.querySelectorAll('.banner__blog-item');
+
+blogList.forEach(item => {
+	item.addEventListener('click', function(event){
+		if(event.target.closest('.banner__blog-item')) {
+			blogList.forEach(item => item.classList.remove('active-blog'));
+			item.classList.add('active-blog');
+		}
 	})
 });
